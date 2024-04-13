ef67cb7ec61f3c37de2a55e93c83355ccdf4f40f
Bryan Fullam <bryanfullam@gmail.com>
2024-04-13 19:44:02 +0200

    Remove YourContract

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "../contracts/YourContract.sol";
 import "./DeployHelpers.s.sol";
 import {MyNFT} from "../contracts/MyNFT.sol";
 import {ERC6551Account} from "../contracts/ERC6551Account.sol";
