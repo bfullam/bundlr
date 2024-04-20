7e0341ff42e80655512f8dcd6ca1995659a4ffaf
Bryan Fullam <bryanfullam@gmail.com>
2024-04-20 20:00:39 +0200

    Add query to sushi subgraph

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,94 @@
 "use client";
 
+import { useState } from "react";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import type { NextPage } from "next";
 import { parseEther } from "viem";
+import { useChainId } from "wagmi";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const chainId = useChainId();
+  const [pairingTokens, setPairingTokens] = useState([]) as any[];
+  const [pairingTokensChainId, setPairingTokensChainId] = useState() as any;
+
+  // Mapping chainId to chainName so we can use the correct subgraph
+  const chainIdToChainName: { [key: number]: string } = {
+    1: "ethereum",
+    31337: "ethereum",
+    100: "gnosis",
+    42161: "arbitrum",
+  };
+
+  // Use the chainIdToChainName mapping to get the correct subgraph for the chainId
+  const APIURL = `https://api.thegraph.com/subgraphs/name/sushi-v3/v3-${chainIdToChainName[chainId]}`;
+
+  // Query to get all the pools on the SushiSwap subgraph
+  const tokensQuery = `
+    query {
+      pools(first: 1000) {
+        id
+        token0 {
+          id
+          symbol
+        }
+        token1 {
+          id
+          symbol
+        }
+        feeTier
+        liquidity
+      }
+    }
+  `;
+
+  // Create a new ApolloClient to fetch data from the subgraph
+  const client = new ApolloClient({
+    uri: APIURL,
+    cache: new InMemoryCache(),
+  });
+
+  // Fetch the data from the subgraph
+  client
+    .query({
+      query: gql(tokensQuery),
+    })
+    .then(data => {
+      const tokensThatPairWithEth = data.data.pools.reduce((acc: any, pool: any) => {
+        // Check if the pool is an ETH pool and has liquidity
+        const isEthPool = pool.token0.symbol === "WETH" || pool.token1 === "WETH";
+        const hasLiquidity = pool.liquidity > 0;
+        if (!isEthPool || !hasLiquidity) {
+          return acc;
+        }
+
+        // Organize the data by token, so we can see all the pools for each token with their liquidity and feeTier
+        const nonEthToken = pool.token0.symbol === "WETH" ? pool.token1 : pool.token0;
+        if (acc[nonEthToken.id]) {
+          return {
+            ...acc,
+            [nonEthToken.id]: {
+              ...acc[nonEthToken.id],
+              pools: [...acc[nonEthToken.id].pools, { feeTier: pool.feeTier, liquidity: pool.liquidity }],
+            },
+          };
+        }
+        return {
+          ...acc,
+          [nonEthToken.id]: { ...nonEthToken, pools: [{ feeTier: pool.feeTier, liquidity: pool.liquidity }] },
+        };
+      }, []);
+
+      // If the pairingTokens array is empty or the chainId has changed, update the state
+      if (pairingTokens.length === 0 || pairingTokensChainId !== chainId) {
+        setPairingTokens(tokensThatPairWithEth);
+        setPairingTokensChainId(chainId);
+      }
+    })
+    .catch(err => {
+      console.log("Error fetching data: ", err);
+    });
+
   const {
     writeAsync: mintNFT,
     // isLoading: mintNFTIsLoading,
@@ -58,6 +142,27 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
           </h1>
+        </div>
+
+        <div className="px-5">
+          <h2 className="text-center text-xl">Available Tokens with their pools</h2>
+          <ul className="text-center">
+            {Object.values(pairingTokens).map((pool: any, index: number) => (
+              <>
+                <li key={index}>
+                  <span>{pool.symbol}</span>
+                  <ul>
+                    {pool.pools.map((pool: any, index: number) => (
+                      <li key={index}>
+                        <span>Fee Tier: {pool.feeTier}</span>, <span>Liquidity: {pool.liquidity}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <br />
+              </>
+            ))}
+          </ul>
         </div>
 
         <button
