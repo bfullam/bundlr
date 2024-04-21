e0fc98310202811722ad1537ee266b41208fcbbe
rickkdev <l.riccardo@web.de>
2024-04-21 10:25:05 +0200

    cleaned up code for linting and added buttons to nft

---
+++ packages/nextjs/pages/api/tokenPrices.ts
@@ -0,0 +1,49 @@
+// pages/api/tokenPrices.ts
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ApiError = {
+  message: string;
+};
+
+type ApiResponse = {
+  status?: string;
+  data?: any; // Define more specifically based on what you expect to receive.
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse | ApiError>) {
+  const { id } = req.query; // Get the id from query parameters
+
+  if (!id) {
+    res.status(400).json({ message: "No ID provided" });
+    return;
+  }
+
+  const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?id=${id}`;
+  const apiKey = process.env.NEXT_PUBLIC_CMC_API_KEY || ""; // Default to an empty string if undefined
+
+  // Check if apiKey is present
+  if (!apiKey) {
+    res.status(500).json({ message: "API key is not properly configured." });
+    return;
+  }
+
+  const headers: HeadersInit = {
+    "X-CMC_PRO_API_KEY": apiKey,
+    Accept: "application/json",
+  };
+
+  try {
+    const apiRes = await fetch(url, {
+      headers,
+    });
+
+    if (!apiRes.ok) {
+      throw new Error(`HTTP error! Status: ${apiRes.status}`);
+    }
+
+    const data: ApiResponse = await apiRes.json();
+    res.status(200).json(data);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message || "Failed to fetch data" });
+  }
+}
